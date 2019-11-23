eeda5f3dcbce45f68fdd2dcb87d83a107f38512d
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-23 14:14:47 -0600

    m5278

---
+++ Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDecking
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblSquareFeet = New System.Windows.Forms.Label()
+        Me.txtFootage = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AccessibleName = "lblHeading"
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Goudy Old Style", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(27, 13)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(337, 37)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Decking Cost Calculator"
+        '
+        'lblSquareFeet
+        '
+        Me.lblSquareFeet.AutoSize = True
+        Me.lblSquareFeet.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSquareFeet.Location = New System.Drawing.Point(88, 50)
+        Me.lblSquareFeet.Name = "lblSquareFeet"
+        Me.lblSquareFeet.Size = New System.Drawing.Size(149, 25)
+        Me.lblSquareFeet.TabIndex = 1
+        Me.lblSquareFeet.Text = "Square Footage:"
+        '
+        'txtFootage
+        '
+        Me.txtFootage.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFootage.Location = New System.Drawing.Point(243, 50)
+        Me.txtFootage.Name = "txtFootage"
+        Me.txtFootage.Size = New System.Drawing.Size(50, 33)
+        Me.txtFootage.TabIndex = 2
+        Me.txtFootage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'frmDecking
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(704, 392)
+        Me.Controls.Add(Me.txtFootage)
+        Me.Controls.Add(Me.lblSquareFeet)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "frmDecking"
+        Me.Text = "Decking Cost Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblSquareFeet As Label
+    Friend WithEvents txtFootage As TextBox
+End Class
