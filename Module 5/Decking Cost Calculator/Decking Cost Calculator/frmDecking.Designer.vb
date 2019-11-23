90a9a62be72d3231207d11f95b6b6c3737468a77
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-23 14:21:10 -0600

    279

--- Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.Designer.vb
+++ Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.Designer.vb
@@ -25,6 +25,11 @@ Partial Class frmDecking
         Me.lblHeading = New System.Windows.Forms.Label()
         Me.lblSquareFeet = New System.Windows.Forms.Label()
         Me.txtFootage = New System.Windows.Forms.TextBox()
+        Me.grpDeckType = New System.Windows.Forms.GroupBox()
+        Me.radLumber = New System.Windows.Forms.RadioButton()
+        Me.radComposite = New System.Windows.Forms.RadioButton()
+        Me.radRedwood = New System.Windows.Forms.RadioButton()
+        Me.grpDeckType.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblHeading
@@ -57,17 +62,67 @@ Partial Class frmDecking
         Me.txtFootage.TabIndex = 2
         Me.txtFootage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
+        'grpDeckType
+        '
+        Me.grpDeckType.BackColor = System.Drawing.Color.AliceBlue
+        Me.grpDeckType.Controls.Add(Me.radComposite)
+        Me.grpDeckType.Controls.Add(Me.radLumber)
+        Me.grpDeckType.Controls.Add(Me.radRedwood)
+        Me.grpDeckType.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpDeckType.Location = New System.Drawing.Point(93, 89)
+        Me.grpDeckType.Name = "grpDeckType"
+        Me.grpDeckType.Size = New System.Drawing.Size(244, 106)
+        Me.grpDeckType.TabIndex = 3
+        Me.grpDeckType.TabStop = False
+        Me.grpDeckType.Text = "Deck Type"
+        '
+        'radLumber
+        '
+        Me.radLumber.AutoSize = True
+        Me.radLumber.Location = New System.Drawing.Point(20, 19)
+        Me.radLumber.Name = "radLumber"
+        Me.radLumber.Size = New System.Drawing.Size(218, 27)
+        Me.radLumber.TabIndex = 0
+        Me.radLumber.TabStop = True
+        Me.radLumber.Text = "Pressure-Treated Lumber"
+        Me.radLumber.UseVisualStyleBackColor = True
+        '
+        'radComposite
+        '
+        Me.radComposite.AutoSize = True
+        Me.radComposite.Location = New System.Drawing.Point(20, 62)
+        Me.radComposite.Name = "radComposite"
+        Me.radComposite.Size = New System.Drawing.Size(111, 27)
+        Me.radComposite.TabIndex = 1
+        Me.radComposite.TabStop = True
+        Me.radComposite.Text = "Composite"
+        Me.radComposite.UseVisualStyleBackColor = True
+        '
+        'radRedwood
+        '
+        Me.radRedwood.AutoSize = True
+        Me.radRedwood.Location = New System.Drawing.Point(20, 41)
+        Me.radRedwood.Name = "radRedwood"
+        Me.radRedwood.Size = New System.Drawing.Size(100, 27)
+        Me.radRedwood.TabIndex = 2
+        Me.radRedwood.TabStop = True
+        Me.radRedwood.Text = "Redwood"
+        Me.radRedwood.UseVisualStyleBackColor = True
+        '
         'frmDecking
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(704, 392)
+        Me.Controls.Add(Me.grpDeckType)
         Me.Controls.Add(Me.txtFootage)
         Me.Controls.Add(Me.lblSquareFeet)
         Me.Controls.Add(Me.lblHeading)
         Me.Name = "frmDecking"
         Me.Text = "Decking Cost Calculator"
+        Me.grpDeckType.ResumeLayout(False)
+        Me.grpDeckType.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -76,4 +131,8 @@ Partial Class frmDecking
     Friend WithEvents lblHeading As Label
     Friend WithEvents lblSquareFeet As Label
     Friend WithEvents txtFootage As TextBox
+    Friend WithEvents grpDeckType As GroupBox
+    Friend WithEvents radLumber As RadioButton
+    Friend WithEvents radRedwood As RadioButton
+    Friend WithEvents radComposite As RadioButton
 End Class
--- Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.vb
+++ Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.vb
@@ -1,3 +1,5 @@
 ﻿Public Class frmDecking
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles radComposite.CheckedChanged
 
+    End Sub
 End Class
