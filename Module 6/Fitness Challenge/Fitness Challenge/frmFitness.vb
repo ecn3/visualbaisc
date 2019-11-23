1713b4c3256c6a8ae71d1b5995653e4ae5a3e64c
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-23 15:49:03 -0600

    coded

--- Module 6/Fitness Challenge/Fitness Challenge/frmFitness.vb
+++ Module 6/Fitness Challenge/Fitness Challenge/frmFitness.vb
@@ -1,5 +1,71 @@
-﻿Public Class Form1
+﻿'* Program Name: Fitness Challenge
+'* Author: Christian Nelson
+'* Date: 11/23/19
+'* Purpose: This Windows form application c gets and displays weight loss values, as well as average weight loss
+Option Strict On
+Public Class Form1
     Private Sub btnWeightLoss_Click(sender As Object, e As EventArgs) Handles btnWeightLoss.Click
+        'Accept weight entries
 
+        'Initalize
+        Dim strWeightLoss As String
+        Dim decWeightLoss As Decimal
+        Dim decAverageLoss As Decimal
+        Dim decTotalWeightLoss As Decimal = 0D
+        Dim strInputMessage As String = "Enter the weight loss for team member #"
+        Dim strInputHeading As String = "Weight Loss"
+        Dim strNormalMessage As String = "Enter the weight loss for team member #"
+        Dim strNonNumericError As String = "Error - Enter a number for weight loss of team member #"
+        Dim strNegativeError As String = "Error - Enter a  positive number for weight loss of team member #"
+
+        'Dinitalize loop vars
+        Dim strCancelClicked As String = ""
+        Dim intMaxNumberOfEntries As Integer = 8
+        Dim intNumberOfEntries As Integer = 1
+
+        strWeightLoss = InputBox(strInputMessage & intNumberOfEntries, strInputHeading)
+
+        Do Until intNumberOfEntries > intMaxNumberOfEntries Or strWeightLoss = strCancelClicked
+            If IsNumeric(strWeightLoss) Then
+                decWeightLoss = Convert.ToDecimal(strWeightLoss)
+                If decWeightLoss > 0 Then
+                    lstWeightLoss.Items.Add(decWeightLoss)
+                    decTotalWeightLoss += decWeightLoss
+                    intNumberOfEntries += 1
+                    strInputMessage = strNormalMessage
+                Else
+                    strInputMessage = strNegativeError
+                End If
+            Else
+                strInputMessage = strNonNumericError
+            End If
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strWeightLoss = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+            End If
+        Loop
+
+        'calculate and display average
+        If intNumberOfEntries > 1 Then
+            lblAverageLoss.Visible = True
+            decAverageLoss = decTotalWeightLoss / (intNumberOfEntries - 1)
+            lblAverageLoss.Text = "Average Weight loss is " & decAverageLoss.ToString("F1") & " lbs"
+        Else
+            MsgBox("No Weight loss value entered")
+        End If
+
+        'disable weight loss button
+        btnWeightLoss.Enabled = False
     End Sub
+
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        lstWeightLoss.Items.Clear()
+        lblAverageLoss.Visible = False
+        btnWeightLoss.Enabled = True
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        Close()
+    End Sub
+
+
 End Class
