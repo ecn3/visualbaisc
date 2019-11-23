25e72bfca17f1674b042d7a0f9b5df64ff49f150
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-23 14:40:14 -0600

    281

--- Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.vb
+++ Module 5/Decking Cost Calculator/Decking Cost Calculator/frmDecking.vb
@@ -1,5 +1,13 @@
-﻿Public Class frmDecking
-    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles radComposite.CheckedChanged
+﻿'* Program Name: Decking Cost Calculator
+'* Author: Christian Nelson
+'* Date: 11/23/19
+'* Purpose: This Windows form application computes the estimated cost of decking based on the
+'* number of quare feet and the following cost per quare foot: Lumbrt - $2.35
+'* Redwood - $7.75, Composite - $8.50
+Option Strict On
+
+Public Class frmDecking
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
 
     End Sub
 End Class
